f8a41fe7fa2f6541b6f778e82fa00993b6a357e0
tama926 <acta_tamami@icloud.com>
2020-07-04 08:47:05 +0900

    UnityChan テキスト課題

---
+++ Assets/destroyitem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyitem : MonoBehaviour
+{
+    private GameObject unitychan;
+
+    void Start()
+    {
+        //シーン中のunitychanオブジェクトを取得
+        this.unitychan = GameObject.Find("unitychan");
+    }
+    void Update()
+    {
+        //unitychanがアイテムを通り越した場合
+        if (this.transform.position.z < this.unitychan.transform.position.z)
+        {
+            //スクリプトがついているオブジェクトを削除
+            Destroy(gameObject);
+
+        }
+    }
+}
